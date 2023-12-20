a9d574a7838c6f844a87d6f63ec47bf61b9feffe
Thiago Henrique Bortolozo <porrtuguinha@gmail.com>
2023-12-20 18:52:02 -0300

    Responsivo

--- src/components/body/fotos.js
+++ src/components/body/fotos.js
@@ -6,64 +6,64 @@ return(
 <div className="fotos">
         <div className="fotos-coluna1">
             <div className="imagemlado1">
-                <img src="images/laura1.jpg" alt="imagem vô e vó" height="500px" width="450px"/>
+                <img src="images/laura1.jpg" alt="imagem vô e vó" height="100%" width="100%"/>
             </div>
             <div className="imagemlado2">
-                <img src="images/FotoEstiloAntigos.jpg" alt="imagem antiga" height="500px" width="450px"/>
+                <img src="images/FotoEstiloAntigos.jpg" alt="imagem antiga" height="100%" width="100%"/>
             </div>
             <div className="imagemlado3">
-                <img src="images/Romantico.jpg" alt="imagem beijo" height="500px" width="450px"/>
+                <img src="images/Romantico.jpg" alt="imagem beijo" height="100%" width="100%"/>
 
             </div>
 
         </div>
         <div className="fotos-coluna1">
             <div className="imagemlado1">
-                <img src="images/Homens.jpg" alt="Homens da familia" height="500px" width="450px"/>
+                <img src="images/Homens.jpg" alt="Homens da familia" height="100%" width="100%"/>
             </div>
             <div className="imagemlado2">
-                <img src="images/Familia2.jpg" alt="foto da familia com o autor junto" height="500px" width="450px"/>
+                <img src="images/Familia2.jpg" alt="foto da familia com o autor junto" height="100%" width="100%"/>
             </div>
             <div>
-                <img src="images/family.jpeg" height="500" width="450"/>
+                <img src="images/family.jpeg" height="100%" width="100%"/>
             </div>
         </div>
         <div className="fotos-coluna1">
             <div className="imagemlado1">
-                <img src="images/festa.jpg" height="500px" width="450px"></img>
+                <img src="images/festa.jpg" height="100%" width="100%"></img>
             </div>
             <div className="imagemlado2">
-                <img src="images/CasalLindo.jpg" height="500px" width="450px"></img>
+                <img src="images/CasalLindo.jpg" height="100%" width="100%"></img>
             </div>
             <div>
-                <img src="images/neuGaranhão.jpg" height="500px" width="450px"></img>
+                <img src="images/neuGaranhão.jpg" height="100%" width="100%"></img>
             </div>
         </div>
         <div className="fotos-coluna1">
             <div className="imagemlado1">
-                <img src="images/neu.jpg" height="500px" width="450px"></img>
+                <img src="images/neu.jpg" height="100%" width="100%"></img>
             </div>
             <div className="imagemlado2">
-                <img src="images/netos.jpg" height="500px" width="450px"></img>
+                <img src="images/netos.jpg" height="100%" width="100%"></img>
             </div>
             <div>
-                <img src="images/inicio.png" height="500px" width="450px"></img>
+                <img src="images/inicio.png" height="100%" width="100%"></img>
             </div>
         </div>
         <div className="fotos-coluna1">
             <div className="imagemlado1">
-                <img src="images/elegancia.jpg" height="500px" width="450px"></img>
+                <img src="images/elegancia.jpg" height="100%" width="100%"></img>
             </div>
             <div className="imagemlado2">
-                <img src="images/lindinhos.jpg" height="500px" width="450px"></img>
+                <img src="images/lindinhos.jpg" height="100%" width="100%"></img>
             </div>
             <div>
-                <img src="images/onsbu.jpg" height="500px" width="450px"></img>
+                <img src="images/onsbu.jpg" height="100%" width="100%"></img>
             </div>
         </div>
         <div className="fotos-coluna1">
             <div className="imagemlado1">
-                <img src="images/vóFeliz.jpg" height="500px" width="450px"></img>
+                <img src="images/vóFeliz.jpg" height="100%" width="100%"></img>
             </div>
             <div className="imagemlado3">
                 
