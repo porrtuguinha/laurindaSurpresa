e720049c420a30d05e6d042e1126c45e2388fc6e
Thiago Henrique Bortolozo <porrtuguinha@gmail.com>
2024-01-01 18:36:13 -0300

    alteração tamanho

--- src/components/rodape/rodape.js
+++ src/components/rodape/rodape.js
@@ -5,8 +5,12 @@ return(
 <footer className="fim">
         <p className="texto">Uma homenagem de toda a sua linda familia</p>
         <div className="imagemFamilia">
-            <img src="./images/familiaBig.jpg" alt="imagem familia" height="300px" width="500px"/>
+            <img className="foto" src="./images/familiaBig.jpg"  alt="imagem familia" height="100%" width="100%"/>
         </div>
+        <audio autoplay="autoplay" controls="controls">
+<source src="images/manoelOficial.mp3" type="audio/mp3" />
+seu navegador não suporta HTML5
+</audio>
     </footer>
 
 )
