5912e129e1748162753e76ccfe1a1dd6f9d3b20c
thiago henrique bortolozo da silva <porrtuguinha@gmail.com>
2024-01-31 12:20:15 -0300

    estilo do botão

--- src/components/Banner2/ManoelBanner.js
+++ src/components/Banner2/ManoelBanner.js
@@ -2,8 +2,8 @@ import './Banner2.css'
 function ManoelBanner(){
     return(
         <div className="inicioManoel">
-        <h1 className="textoManoel">Fotos do cabeço</h1>
-        <p className="textoManoel">Fotos do lugar onde manoel cresceu e viveu</p>
+        <h1 className="textoManoel">Fotos do Cabeço</h1>
+        <p className="textoManoel">Fotos do lugar onde Manoel cresceu e viveu</p>
 
 
     </div>
--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -4,8 +4,8 @@ var laurinda = new Laurinda;
 function Banner(){
     return(
         <div className="inicioLaurinda">
-        <h1 className="textoLaurinda">Homenagem para Manuel</h1>
-        <p className="textoLaurinda">Uma homenagem da familia silva para o casal Manuel e {laurinda.nome}</p>
+        <h1 className="textoLaurinda">Homenagem para Manoel</h1>
+        <p className="textoLaurinda">Uma homenagem da familia silva para o casal Manoel e {laurinda.nome}</p>
 
 
     </div>
