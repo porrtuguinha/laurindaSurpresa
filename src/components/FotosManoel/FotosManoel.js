0323232c22643a48e80256f8c27c5e23e9cd000b
Thiago Henrique Bortolozo <porrtuguinha@gmail.com>
2024-03-10 20:03:58 -0300

    ponte de sor

--- src/components/FotosManoel/FotosManoel.js
+++ src/components/FotosManoel/FotosManoel.js
@@ -28,7 +28,44 @@ return(
                 <img src="images/rua.jpeg" alt="" height="100%" width="100%"/>
 
             </div>
+            
 
+        </div>
+        <div className="fotos-coluna1">
+            <div className="imagemlado1">
+                <img src="images/arte.jpeg" alt="" height="100%" width="100%"/>
+            </div>
+            <div className="imagemlado2">
+                <img src="images/casa.jpeg" alt="" height="100%" width="100%"/>
+            </div>
+            <div className="imagemlado3">
+                <img src="images/igreja.jpeg" alt="" height="100%" width="100%"/>
+
+            </div>
+            
+
+        </div>
+        <div className="fotos-coluna1">
+            <div className="imagemlado1">
+                <img src="images/moto.jpeg" alt="" height="100%" width="100%"/>
+            </div>
+            <div className="imagemlado2">
+                <img src="images/paisagem.jpeg" alt="" height="100%" width="100%"/>
+            </div>
+            <div className="imagemlado3">
+                <img src="images/paisagem2.jpeg" alt="" height="100%" width="100%"/>
+
+            </div>
+            
+
+        </div>
+        <div className="fotos-coluna1">
+            <div className="imagemlado1">
+                <img src="images/poste.jpeg" alt="" height="100%" width="100%"/>
+            </div>
+            <div className="imagemlado2">
+                <img src="images/ruaigreja.jpeg" alt="" height="100%" width="100%"/>
+            </div>
         </div>
 </div>
         )
